3a4dc5d644104da90b3c88bef8b233f41797c98d
shauncoding28 <196330731+shauncoding28@users.noreply.github.com.>
2025-03-11 19:02:16 +0100

    create get request/add features

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var app = express();
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// app.set('trust proxy', true); // used to ensure .ip returns original server and not proxy
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -22,6 +23,14 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+  const clientIp = req.ip;
+  const preferredLanguage = req.headers['accept-language'];
+  const clientSoftware = req.headers['user-agent'];
+
+  res.status(200).json({ ip: clientIp, language: preferredLanguage, software: clientSoftware });
 });
 
 // listen for requests :)
